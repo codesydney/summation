20084c4c9a4be44440ebf506a52171da53d0a457
Rafilkis <rafaelstanzione@gmail.com>
2017-07-24 17:18:58 +1000

    updating Flask folder
    updating Flask folder

--- flask/Scripts/Activate.ps1
+++ flask/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\summation\flask"
+$env:VIRTUAL_ENV="C:\Users\Rafilkis\AppData\Local\GitHubDesktop\Repository\summation\flask"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- flask/Scripts/activate.bat
+++ flask/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\summation\flask"
+set "VIRTUAL_ENV=C:\Users\Rafilkis\AppData\Local\GitHubDesktop\Repository\summation\flask"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
